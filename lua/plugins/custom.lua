1f4881a96b9adcc275d642f2ddb7f1512e23e94a
toriumi0118 <toriumi0118@gmail.com>
2025-05-19 14:51:57 +0900

    fix:

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -6,38 +6,32 @@ return {
         enabled = false,
       },
       servers = {
-        yamlls = {
-          settings = {
-            format = {
-              singleQuote = false,
-            },
-          },
-        },
+        eslint = {},
+        yamlls = { settings = { yaml = { format = { singleQuote = true } } } },
+      },
+      setup = {
+        eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            elseif client.name == "vtsls" then
+              client.server_capabilities.documentFormattingProvider = false
+            elseif client.name == "ts-ls" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
       },
     },
-    -- opts = {
-    --   -- https://www.lazyvim.org/configuration/recipes#add-eslint-and-use-it-for-formatting
-    --   servers = {
-    --     eslint = {},
-    --   },
-    --   setup = {
-    --     eslint = function()
-    --       require("lazyvim.util").lsp.on_attach(function(client)
-    --         if client.name == "eslint" then
-    --           client.server_capabilities.documentFormattingProvider = true
-    --         elseif client.name == "tsserver" then
-    --           client.server_capabilities.documentFormattingProvider = false
-    --         end
-    --       end)
-    --     end,
-    --   },
-    -- },
   },
   {
     "blink.cmp",
     dependencies = { { "mlaursen/vim-react-snippets" } },
     opts = function(_, opts)
       require("vim-react-snippets").lazy_load()
+      opts.snippets.preset = "luasnip"
     end,
   },
   {
@@ -85,13 +79,13 @@ return {
       },
     },
   },
-  -- {
-  --   "copilot.lua",
-  --   config = function(_, opts)
-  --     require("copilot").setup({
-  --       -- copilot_node_command = "/Users/toriumi0118/.local/share/mise/installs/node/22.14.0/bin/node",
-  --       copilot_node_command = vim.fn.expand("$HOME") .. "/.local/share/mise/installs/node/22.14.0/bin/node",
-  --     })
-  --   end,
-  -- },
+  {
+    "copilot.lua",
+    config = function(_, opts)
+      require("copilot").setup({
+        -- copilot_node_command = "/Users/toriumi0118/.local/share/mise/installs/node/22.14.0/bin/node",
+        copilot_node_command = vim.fn.expand("$HOME") .. "/.local/share/mise/installs/node/22.14.0/bin/node",
+      })
+    end,
+  },
 }
