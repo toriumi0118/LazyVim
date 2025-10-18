1f6c30420d098fc4e13457a5b174c581d8ee09fe
toriumi0118 <toriumi0118@gmail.com>
2025-10-18 17:31:36 +0900

    fix: use extras

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -6,24 +6,24 @@ return {
         enabled = false,
       },
       servers = {
-        eslint = {},
-        tsserver = {},
+        -- eslint = {},
+        -- tsserver = {},
         yamlls = { settings = { yaml = { format = { singleQuote = true } } } },
       },
       setup = {
-        eslint = function()
-          require("lazyvim.util").lsp.on_attach(function(client)
-            if client.name == "eslint" then
-              client.server_capabilities.documentFormattingProvider = true
-            elseif client.name == "tsserver" then
-              client.server_capabilities.documentFormattingProvider = false
-            elseif client.name == "vtsls" then
-              client.server_capabilities.documentFormattingProvider = false
-            elseif client.name == "ts-ls" then
-              client.server_capabilities.documentFormattingProvider = false
-            end
-          end)
-        end,
+        -- eslint = function()
+        --   require("lazyvim.util").lsp.on_attach(function(client)
+        --     if client.name == "eslint" then
+        --       client.server_capabilities.documentFormattingProvider = true
+        --     elseif client.name == "tsserver" then
+        --       client.server_capabilities.documentFormattingProvider = false
+        --     elseif client.name == "vtsls" then
+        --       client.server_capabilities.documentFormattingProvider = false
+        --     elseif client.name == "ts-ls" then
+        --       client.server_capabilities.documentFormattingProvider = false
+        --     end
+        --   end)
+        -- end,
       },
     },
   },
