3e52259e6513949940430528321e046ecb443ef2
toriumi0118 <toriumi0118@gmail.com>
2026-03-09 23:24:41 +0900

    fix: prettier on save

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,3 +7,23 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- Disable spell checking for markdown files
 vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- ESLint: formatter優先度設定 + save時 --fix
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if not client then return end
+    if client.name == "eslint" then
+      client.server_capabilities.documentFormattingProvider = true
+    elseif client.name == "vtsls" or client.name == "ts_ls" then
+      client.server_capabilities.documentFormattingProvider = false
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.ts", "*.tsx", "*.js", "*.jsx" },
+  callback = function()
+    vim.lsp.buf.format({ name = "eslint", async = false })
+  end,
+})
--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -6,24 +6,8 @@ return {
         enabled = false,
       },
       servers = {
-        -- eslint = {},
-        -- tsserver = {},
+        eslint = {},
         yamlls = { settings = { yaml = { format = { singleQuote = true } } } },
-      },
-      setup = {
-        -- eslint = function()
-        --   require("lazyvim.util").lsp.on_attach(function(client)
-        --     if client.name == "eslint" then
-        --       client.server_capabilities.documentFormattingProvider = true
-        --     elseif client.name == "tsserver" then
-        --       client.server_capabilities.documentFormattingProvider = false
-        --     elseif client.name == "vtsls" then
-        --       client.server_capabilities.documentFormattingProvider = false
-        --     elseif client.name == "ts-ls" then
-        --       client.server_capabilities.documentFormattingProvider = false
-        --     end
-        --   end)
-        -- end,
       },
     },
   },
